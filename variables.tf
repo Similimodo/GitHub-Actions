aadcf5fc1585db054e6cd56ab79742229f50e0a4
Paschal Onor <paschalonor@gmail.com>
2023-12-10 21:26:45 -0500

    initial

--- variables.tf
+++ variables.tf
@@ -6,14 +6,14 @@ variable "resource_group_name" {
   description = "Name of the resource group."
 }
 
-variable "storage_account_name" {
-  description = "Name of the Storage Account."
-}
+# variable "storage_account_name" {
+#   description = "Name of the Storage Account."
+# }
 
-variable "source_content" {
-  description = "Name of the source content."
-}
+# variable "source_content" {
+#   description = "Name of the source content."
+# }
 
-variable "index_document" {
-  description = "Name of the index document."
-}
+# variable "index_document" {
+#   description = "Name of the index document."
+# }
