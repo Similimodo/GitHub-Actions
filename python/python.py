ca0c55258fefa9467012f60b139ea8b449149f71
Paschal Onor <paschalonor@gmail.com>
2025-05-05 09:08:19 -0400

    Attentive interview

--- python/python.py
+++ python/python.py
@@ -7,6 +7,7 @@ print("")
 
 
 # Variable is a container of value (str, int, float, boolean)
+# String Variables
 first_name = "Bro"
 food = "Moringa"
 email = "paschal@fake.com"
@@ -16,10 +17,30 @@ print(f"You really like {food}")
 print(f"Your email is {email}")
 
 
+print("************************")
+print("")
+
+
+# Integer 
+age = 37
+num_of_students = 99
+
+print(f"You are {age} years old")
+print(f"The total number of students is {num_of_students}")
+
 
 print("************************")
 print("")
 
+
+# Float
+price = 10.99
+
+print(f"The price is: {price}")
+
+
+print("************************")
+print("")
 
 # Input
 Name = input("What is your name?  ")
