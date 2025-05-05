bcdeeb85cd50b0442e265cacf7dbb08ce3c7b463
Paschal Onor <paschalonor@gmail.com>
2025-05-05 12:02:00 -0400

    Attentive interview

--- python/python.py
+++ python/python.py
@@ -35,12 +35,36 @@ print("")
 
 # Float
 price = 10.99
+gpa = 3.5
+distance = 6.7
 
 print(f"The price is: {price}")
-
+print(f"My gpa is: {gpa}")
+print(f"You ran {distance}km")
 
 print("************************")
 print("")
+
+# Boolean
+is_student = True
+for_sale = True
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are Not a student")
+
+print("************************")
+print("")
+
+if for_sale:
+    print("This item is for sale.")
+else:
+    print("This item is Not for sale.")
+
+print("************************")
+print("")
+
 
 # Input
 Name = input("What is your name?  ")
---
+++ python/typecasting.py
@@ -0,0 +1,12 @@
+arrayA = [2,4,6,8,10,12,14]
+arrayB = [1,3,6,5,7,9,14]
+
+def mergeArray (arrA, arrB):
+    return set(arrA + arrB)
+
+print(mergeArray(arrayA, arrayB))
+
+
+# total = set(arrayA + arrayB)
+
+# print(total)
