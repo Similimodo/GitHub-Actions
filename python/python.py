59ba7d399153c4295c81e0e2164ba3ee0c07555d
Paschal Onor <paschalonor@gmail.com>
2025-05-04 21:05:38 -0400

    Attentive interview

--- python/python.py
+++ python/python.py
@@ -1,5 +1,19 @@
-#Normal print
+# Normal print
 print("Paschal is a good boy")
+
+
+print("************************")
+print("")
+
+
+# Variable is a container of value (str, int, float, boolean)
+first_name = "Bro"
+print(first_name)
+
+
+print("************************")
+print("")
+
 
 # Input
 Name = input("What is your name?  ")
