97c3837e3e015bf240673f676831182cdd0f0ffd
Paschal Onor <paschalonor@gmail.com>
2025-05-04 20:52:56 -0400

    Attentive interview

---
+++ python/python.py
@@ -0,0 +1,9 @@
+#Normal print
+print("Paschal is a good boy")
+
+# Input
+Name = input("What is your name?  ")
+Address = input("what is your current address?  ")
+
+print(f"My name is {Name} and I live at {Address}")
+
