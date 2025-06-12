9382406d8ae4df044270f5072feaf28a35cd9e0f
Similimodo <paschalonor@gmail.com>
2025-06-11 20:25:46 -0400

    Update python

---
+++ python/for_loop.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# For loop = Executes a block of code a fixed number of times.
+#            You can iterate over a string, range or sequence etc.
+
+for i in range(1, 11):
+    print(i)
