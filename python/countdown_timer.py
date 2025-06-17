d3742dcc389ac4029c585c869e4cecfb4dbe777b
Paschal Onor <paschalonor@gmail.com>
2025-06-16 22:05:34 -0400

    another one

--- python/countdown_timer.py
+++ python/countdown_timer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-
 import time
 
+my_time = int(input("Enter timer in seconds: "))
+
+
+
 print("Starting Timer Now!")
-time.sleep(4)
+for a in range(0, my_time):
+    print(a)
+    time.sleep(4)
 
 print("Time's Up!")
